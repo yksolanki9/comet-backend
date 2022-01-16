0476ba2f6b91ab90177a06dc623d3a6e6ce21ca5
Yash <yashsolanki1709@gmail.com>
2022-01-16 20:57:25 +0530

    Update env vars'

--- mongoose.js
+++ mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { MONGO_USER, MONGO_PASSWORD } = process.env;
 
-mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@comet-notes.gplt8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CLUSTER}.mongodb.net/${MONGO_DBNAME}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
