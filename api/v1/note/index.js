46ead6b4f99054bbfe467450f3d8b3283c4e98f1
yksolanki9 <yashsolanki1709@gmail.com>
2022-12-25 13:11:15 +0530

    Block access to notes API without access token

--- api/v1/note/index.js
+++ api/v1/note/index.js
@@ -1,97 +1,103 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const verifyToken = require("../../../middleware/auth");
 const router = express.Router();
-const Note = require('../../../models/note.model');
-const UserNotes = require('../../../models/user-notes.model');
+const Note = require("../../../models/note.model");
+const UserNotes = require("../../../models/user-notes.model");
 
-router.post('/', async (req, res) => {
+router.post("/", verifyToken, async (req, res) => {
   try {
     const note = new Note({
       _id: new mongoose.Types.ObjectId(),
       userId: req.user.userId,
-      ...req.body
+      ...req.body,
     });
     await note.save(async (err) => {
       if (err) {
         return res.status(500).send(err);
       }
       try {
-        const userNote = await UserNotes.findOneAndUpdate({userId: req.user.userId}, {
-          '$push': { 'notes': note._id}
-        });
-      
+        const userNote = await UserNotes.findOneAndUpdate(
+          { userId: req.user.userId },
+          {
+            $push: { notes: note._id },
+          }
+        );
+
         if (!userNote) {
           const newUserNote = new UserNotes({
             userId: req.user.userId,
-            notes: [note._id]
-          })
+            notes: [note._id],
+          });
           await newUserNote.save();
         }
-      }
-      catch (err) {
-        return res.status(500).send(err.message); 
+      } catch (err) {
+        return res.status(500).send(err.message);
       }
       return res.status(200).send(note);
     });
-  } catch(err) {
+  } catch (err) {
     res.status(500).send({
       status: 500,
-      message: err.message
+      message: err.message,
     });
   }
-})
+});
 
-router.get('/:noteId', async (req, res) => {
+router.get("/:noteId", verifyToken, async (req, res) => {
   try {
     const note = await Note.findById(req.params.noteId);
     if (!note) {
-      return res.status(404).send('Note with id does not exist');
+      return res.status(404).send("Note with id does not exist");
     }
     if (note.userId.toString() !== req.user.userId) {
-      return res.status(403).send('Unauthorized to access data');
+      return res.status(403).send("Unauthorized to access data");
     }
     return res.status(200).send(note);
-  } catch(err) {
+  } catch (err) {
     res.status(500).send({
       status: 500,
-      message: err.message
+      message: err.message,
     });
   }
 });
 
-router.patch('/:noteId', async (req, res) => {
+router.patch("/:noteId", verifyToken, async (req, res) => {
   try {
     const note = await Note.findByIdAndUpdate(req.params.noteId, req.body);
     if (!note) {
-      return res.status(404).send('Note with id does not exist');
+      return res.status(404).send("Note with id does not exist");
     }
     if (note.userId.toString() !== req.user.userId) {
-      return res.status(403).send('Unauthorized to access data');
+      return res.status(403).send("Unauthorized to access data");
     }
     return res.status(200).send(note);
-  } catch(err) {
+  } catch (err) {
     res.status(500).send({
       status: 500,
-      message: err.message
+      message: err.message,
     });
   }
 });
 
-router.delete('/:noteId', async (req, res) => {
+router.delete("/:noteId", verifyToken, async (req, res) => {
   try {
     const note = await Note.findByIdAndDelete(req.params.noteId);
     if (!note) {
-      return res.status(404).send('Note with id does not exist');
+      return res.status(404).send("Note with id does not exist");
     }
-    await UserNotes.findOneAndUpdate({ userId: req.user.userId}, {
-      '$pull': {'notes': req.params.noteId}
-    });
-    return res.status(200).send('Note deleted successfully');
-  } catch(err) {
+    await UserNotes.findOneAndUpdate(
+      { userId: req.user.userId },
+      {
+        $pull: { notes: req.params.noteId },
+      }
+    );
+    return res.status(200).send("Note deleted successfully");
+  } catch (err) {
     res.status(500).send({
       status: 500,
-      message: err.message
-    })
+      message: err.message,
+    });
   }
 });
 
--- api/v1/notes/index.js
+++ api/v1/notes/index.js
@@ -1,22 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Note = require('../../../models/note.model');
-const UserNotes = require('../../../models/user-notes.model');
+const Note = require("../../../models/note.model");
+const UserNotes = require("../../../models/user-notes.model");
+const verifyToken = require("../../../middleware/auth");
 
-router.get('/', async (req, res) => {
+router.get("/", verifyToken, async (req, res) => {
   try {
     await UserNotes.findOne({
-      userId: req.user.userId
-    }).populate('notes').exec((err, userNotes) => {
-      if (err) {
-        return res.status(500).send('Some issue at backend');
-      }
-      return res.status(200).send(userNotes);
-    });
-  } catch(err) {
+      userId: req.user.userId,
+    })
+      .populate("notes")
+      .exec((err, userNotes) => {
+        if (err) {
+          return res.status(500).send("Some issue at backend");
+        }
+        return res.status(200).send(userNotes);
+      });
+  } catch (err) {
     res.status(500).send({
       status: 500,
-      message: err.message
+      message: err.message,
     });
   }
 });
