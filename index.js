158bedcea20b54d18355b061e01d7869029b3c97
yksolanki9 <yashsolanki1709@gmail.com>
2022-12-26 19:38:49 +0530

    Remove logic for token expiry

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const bcrypt = require('bcrypt');
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const User = require('./models/user.model');
-const auth = require('./middleware/auth');
-const cors = require('cors');
+const bcrypt = require("bcrypt");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("./models/user.model");
+const auth = require("./middleware/auth");
+const cors = require("cors");
 
-require('dotenv').config();
-require('./config/mongoose');
+require("dotenv").config();
+require("./config/mongoose");
 
 const app = express();
 
@@ -14,19 +14,19 @@ app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
 
-app.use('/api/v1', auth, require('./api/v1'));
+app.use("/api/v1", auth, require("./api/v1"));
 
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
 
     if (!(firstName && lastName && email && password)) {
-      return res.status(400).send('All inputs are mandatory');
+      return res.status(400).send("All inputs are mandatory");
     }
 
     // Check if user already exists
-    if (await User.findOne({email})) {
-      return res.status(409).send('User with entered email already exists');
+    if (await User.findOne({ email })) {
+      return res.status(409).send("User with entered email already exists");
     }
 
     // Generate pwd hash with 10 saltRounds
@@ -37,63 +37,64 @@ app.post('/register', async (req, res) => {
       firstName,
       lastName,
       email: email.toLowerCase(),
-      password: hashedPassword
+      password: hashedPassword,
     });
     const savedUser = await newUser.save();
 
-    const accessToken = jwt.sign({
-      userId: savedUser._id,
-      email
-    }, process.env.TOKEN_SECRET, {
-      expiresIn: '1h'
-    });
+    const accessToken = jwt.sign(
+      {
+        userId: savedUser._id,
+        email,
+      },
+      process.env.TOKEN_SECRET
+    );
 
     return res.status(201).send({
-      message: 'User registered successfully',
+      message: "User registered successfully",
       userId: savedUser._id,
       email,
-      access_token: accessToken
+      access_token: accessToken,
     });
-  } catch(err) {
+  } catch (err) {
     console.log(err);
   }
 });
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   if (!(email && password)) {
-    return res.status(400).send('All inputs are mandatory');
+    return res.status(400).send("All inputs are mandatory");
   }
 
   //Check if user is registered
-  const user = await User.findOne({email});
-  if(!user) {
-    return res.status(401).send('No user exists with the entered email');
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(401).send("No user exists with the entered email");
   }
 
   //Check if password is correct
-  if(!(await bcrypt.compare(password, user.password))) {
-    return res.status(401).send('Incorrect password');
-  };
+  if (!(await bcrypt.compare(password, user.password))) {
+    return res.status(401).send("Incorrect password");
+  }
 
-  const accessToken = jwt.sign({
-    userId: user._id,
-    email
-  }, process.env.TOKEN_SECRET, {
-    expiresIn: '1h'
-  });
+  const accessToken = jwt.sign(
+    {
+      userId: user._id,
+      email,
+    },
+    process.env.TOKEN_SECRET
+  );
 
   return res.status(200).send({
-    message: 'Logged in successfully',
+    message: "Logged in successfully",
     userId: user._id,
     email,
-    access_token: accessToken
+    access_token: accessToken,
   });
 });
 
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Server running on port', PORT);
+  console.log("Server running on port", PORT);
 });
