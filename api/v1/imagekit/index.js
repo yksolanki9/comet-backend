15c16ea9ceca053e25081aa0e0054761930c878b
yksolanki9 <yashsolanki1709@gmail.com>
2023-01-17 18:09:04 +0530

    Add endpoint for image uploads and increase server request size to 10mb

--- api/v1/imagekit/index.js
+++ api/v1/imagekit/index.js
@@ -5,15 +5,27 @@ const router = express.Router();
 const ImageKit = require("imagekit");
 require("dotenv").config();
 
-router.get("/", verifyToken, (req, res) => {
-  const imagekit = new ImageKit({
-    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
-  });
+const imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+});
 
-  const authenticationParameters = imagekit.getAuthenticationParameters();
-  res.send(authenticationParameters);
+router.post("/", verifyToken, async (req, res) => {
+  try {
+    await imagekit.upload({
+      file: req.body.base64,
+      fileName: req.body.fileName,
+    });
+    res.status(201).send({
+      message: "Image uploaded successfully",
+    });
+  } catch (err) {
+    res.status(500).send({
+      message: "Image Upload failed",
+      error: err,
+    });
+  }
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.urlencoded());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
 app.use("/api/v1", auth, require("./api/v1"));
 
